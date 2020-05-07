4bf4fdf082f12a6225be2ee00aac8d40589048e8
Elise Myklebust <elisemykle@hotmail.com>
2020-05-07 20:06:45 +0200

    Forandret på navbar

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {
   Route,
   Switch
 } from "react-router-dom";
+import Navigation from './components/Navigation.js';
 import Home from "./components/visitor/home/Home.js";
 import Contact from "./components/visitor/contact/Contact.js";
 import Enquiry from "./components/visitor/enquiry/Enquiry.js";
@@ -18,21 +19,11 @@ import Message from "./components/admin/message/Message.js";
 function App() {
   return (
       <Router className="App">
-           <nav className="nav__bar">
-              <NavLink to="/" exact>
-              </NavLink>
-              <button className="nav__menuToggler"/>
-              <div className="nav__right">
-                  <NavLink to="/" exact activeClassName="nav--link__active" className="nav--link">Home</NavLink>
-                  <NavLink to="/Hotels" exact exact activeClassName="nav--link__active" className="nav--link">Hotels</NavLink>
-                  <NavLink to="/Contact" exact exact activeClassName="nav--link__active" className="nav--link">Contact</NavLink>
-                  <NavLink to="/Login" exact exact activeClassName="nav--link__active" className="nav--link">Login</NavLink>
-              </div>
-          </nav>
-          <div className="nav--container">
-                <div className="hero--heading">
+           <Navigation />
+          <div className="hero">
+                <div className="hero__heading">
                     <h1>Stop paying more than other hotel guests</h1>
-                    <p className="hero--subheading">Find the best hotels, b&b’s and guesthouses in Bergen City.</p>
+                    <p className="hero__subheading">Find the best hotels, b&b’s and guesthouses in Bergen City.</p>
                 </div>
           </div>
           <Switch>
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, {useState} from "react";
+import {NavLink} from "react-router-dom";
 
 export default function Navigation(props) {
-    return (
-        <nav className="nav__bar">
-           <NavLink to="/" exact>
-           </NavLink>
-           <button className="nav__menuToggler"/>
-           <div className="nav__right">
-               <NavLink to="/" exact activeClassName="nav--link__active" className="nav--link">Home</NavLink>
-               <NavLink to="/Hotels" exact exact activeClassName="nav--link__active" className="nav--link">Hotels</NavLink>
-               <NavLink to="/Contact" exact exact activeClassName="nav--link__active" className="nav--link">Contact</NavLink>
-               <NavLink to="/Login" exact exact activeClassName="nav--link__active" className="nav--link">Login</NavLink>
-           </div>
-       </nav>
-    )
+    const [hamburger, setHamburger] = useState(false);
+    if (hamburger) {
+        return (
+            <nav className="nav--mobile">
+               <NavLink to="/" exact>
+               </NavLink>
+               <button className="nav__menuToggler--mobile" onClick={() => setHamburger(false)}/>
+               <div className="nav__right--mobile">
+                   <NavLink to="/" exact activeClassName="nav__link--active" className="nav__link--mobile">Home</NavLink>
+                   <NavLink to="/Hotels" exact exact activeClassName="nav__link--active" className="nav__link--mobile">Hotels</NavLink>
+                   <NavLink to="/Contact" exact exact activeClassName="nav__link--active" className="nav__link--mobile">Contact</NavLink>
+                   <NavLink to="/Login" exact exact activeClassName="nav__link--active" className="nav__link--mobile">Login</NavLink>
+               </div>
+            </nav>
+        )
+    } else{
+        return (
+            <nav className="nav">
+               <NavLink to="/" exact>
+               </NavLink>
+               <button className="nav__menuToggler" onClick={() => setHamburger(true)}/>
+               <div className="nav__right">
+                   <NavLink to="/" exact activeClassName="nav__link--active" className="nav__link">Home</NavLink>
+                   <NavLink to="/Hotels" exact exact activeClassName="nav__link--active" className="nav__link">Hotels</NavLink>
+                   <NavLink to="/Contact" exact exact activeClassName="nav__link--active" className="nav__link">Contact</NavLink>
+                   <NavLink to="/Login" exact exact activeClassName="nav__link--active" className="nav__link">Login</NavLink>
+               </div>
+           </nav>
+        )
+    }
 }
