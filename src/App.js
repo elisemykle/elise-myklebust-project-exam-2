385126c18d0739069da517ac61dbedbd0662bd40
Elise Myklebust <elisemykle@hotmail.com>
2020-05-07 18:05:49 +0200

    Forandret navbar koden

--- src/App.js
+++ src/App.js
@@ -19,45 +19,32 @@ import Message from "./components/admin/message/Message.js";
 
 function App() {
   return (
-      <div className="home__page">
-         <Router className="home__layout">
-         <div className="nav__container">
-           <Navbar.Brand>HOLIDAZE tourism agency</Navbar.Brand>
-             <div className="hero__heading">
-                 <h1>Stop paying more than other hotel guests</h1>
-                 <h4 className="hero__subheading">Find the best hotels, b&b’s and guesthouses in Bergen City.</h4>
-             </div>
-         </div>
-         <Navbar className="nav__bar" bg="transparent" variant="light" expand="lg">
-           <Navbar.Toggle className="custom-toggler" aria-controls="basic__navbar-nav" />
-           <Navbar.Collapse className="custom-collapse" id="basic__navbar-nav">
-             <Nav className="ml-auto">
-               <NavLink to="/" exact activeClassName="nav__link--active" className="nav__link">Home</NavLink>
-               <NavLink to="/Hotels" exact activeClassName="nav__link--active" className="nav__link">Hotels</NavLink>
-               <NavLink to="/Contact" exact activeClassName="nav__link--active" className="nav__link">Contact</NavLink>
-               <NavLink to="/Login" exact activeClassName="nav__link--active" className="nav__link">Login</NavLink>
-             </Nav>
-           </Navbar.Collapse>
-         </Navbar>
-           <Container>
-             <Switch>
-               <Route exact path="/">
-                 <Home />
-               </Route>
-               <Route exact path="/Hotels">
-                 <Hotels />
-               </Route>
-               <Route exact path="/Contact">
-               <Contact/>
-               </Route>
-               <Route exact path="/Login">
-                 <Login/>
-               </Route>
-             </Switch>
-         </Container>
-     </Router>
- </div>
-  );
-}
+      <Router className="App">
+          <Navbar bg="dark" variant="dark" expand="lg">
+              <NavLink to="/" exact>
+                  <Navbar.Brand>Holidaze - tourism agency</Navbar.Brand>
+              </NavLink>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                  <Nav className="mr-auto">
+                      <NavLink to="/" exact className="nav-link">Home</NavLink>
+                      <NavLink to="/Hotels" className="nav-link">Hotels</NavLink>
+                          <NavLink to="/Contact" exact className="nav-link">Contact</NavLink>
+                          <NavLink to="/Login" className="nav-link">Login</NavLink>
+                  </Nav>
+              </Navbar.Collapse>
+          </Navbar>
+          <Container>
+              <Switch>
+                  <Route path="/" exact component={Home}>
+                  </Route>
+                  <Route path="/Hotels" component={Hotels}/>
+                  <Route path="/Contact" component={Contact}/>
+                  <Route path="/Login" component={Login}/>
+              </Switch>
+          </Container>
+      </Router>
+      );
+    }
 
 export default App;
