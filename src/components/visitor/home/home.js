3f7412abc00ab687604946e5ff3c1b7ac25dff14
Elise Myklebust <elisemykle@hotmail.com>
2020-05-08 20:06:48 +0200

    footer sticky

--- src/components/visitor/home/home.js
+++ src/components/visitor/home/home.js
@@ -1,19 +1,9 @@
 import React from 'react';
-import { Col, Row, Image, Container} from 'react-bootstrap';
-import aboutbergenImg from "../../../image/aboutbergen.png"
-
 
 export default function Home(props) {
     return (
-        <Container>
-              <Row>
-                <Col xs={12} md={6}>
+    <div className="page">
                     <h1 className="home--h1">About Bergen city</h1>
-                    <Image src={aboutbergenImg} className="about--bergen" alt="aboutbergenimg" fluid/>
-                        <p className="aboutbergen--text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum in leo turpis. Fusce quis enim ante. Etiam cursus nibh sit amet massa tempus ultrices. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Maecenas eget consectetur nulla. Maecenas nec rhoncus urna, volutpat lobortis neque. Aenean sapien dui, scelerisque at mi id, feugiat tincidunt arcu. Curabitur lobortis felis sit amet efficitur cursus.</p>
-                        <p className="aboutbergen--text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum in leo turpis. Fusce quis enim ante. Etiam cursus nibh sit amet massa tempus ultrices. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Maecenas eget consectetur nulla. Maecenas nec rhoncus urna, volutpat lobortis neque. Aenean sapien dui, scelerisque at mi id, feugiat tincidunt arcu. Curabitur lobortis felis sit amet efficitur cursus.</p>
-                </Col>
-              </Row>
-            </Container>
+        </div>
     )
 }
