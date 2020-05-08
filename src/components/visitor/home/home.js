a1b977897c3bf42c8670a04ca4a1c088302277bf
Elise Myklebust <elisemykle@hotmail.com>
2020-05-08 16:29:21 +0200

    Lager footer

--- src/components/visitor/home/home.js
+++ src/components/visitor/home/home.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Col, Row, Image, Container} from 'react-bootstrap';
+import aboutbergenImg from "../../../image/aboutbergen.png"
 
 
 export default function Home(props) {
     return (
-            <h1 className="home--h1">About Bergen City</h1>
-
+        <Container>
+              <Row>
+                <Col xs={12} md={6}>
+                    <h1 className="home--h1">About Bergen city</h1>
+                    <Image src={aboutbergenImg} className="about--bergen" alt="aboutbergenimg" fluid/>
+                        <p className="aboutbergen--text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum in leo turpis. Fusce quis enim ante. Etiam cursus nibh sit amet massa tempus ultrices. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Maecenas eget consectetur nulla. Maecenas nec rhoncus urna, volutpat lobortis neque. Aenean sapien dui, scelerisque at mi id, feugiat tincidunt arcu. Curabitur lobortis felis sit amet efficitur cursus.</p>
+                        <p className="aboutbergen--text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum in leo turpis. Fusce quis enim ante. Etiam cursus nibh sit amet massa tempus ultrices. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Maecenas eget consectetur nulla. Maecenas nec rhoncus urna, volutpat lobortis neque. Aenean sapien dui, scelerisque at mi id, feugiat tincidunt arcu. Curabitur lobortis felis sit amet efficitur cursus.</p>
+                </Col>
+              </Row>
+            </Container>
     )
 }
