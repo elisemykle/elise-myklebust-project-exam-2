c5a043a8b84446ecd9f6e6e2ab95878211205442
Elise Myklebust <elisemykle@hotmail.com>
2020-05-18 23:28:09 +0200

    stilet ferdig et card hotel

--- src/components/visitor/hotels/hotels.js
+++ src/components/visitor/hotels/hotels.js
@@ -21,6 +21,18 @@ const API_URL = "http://localhost:8888/get-establishments.php";
                     <div className="hotels">
                         <Hero title="The Hotels" text="Choose between your hotels, b&b’s and guesthouses to stay at." classes="hero" />
 
+                        <div className="card">
+                                <img className="card__image" src="https://media-cdn.tripadvisor.com/media/photo-s/11/d0/74/4a/sunset-beach-hotel.jpg" alt="hotel" />
+                            <div className="card__body">
+                                <h2 className="card__title"> Sunset Beach</h2>
+                                <hr className="card__line"/>
+                                <p className="card__description">Get ready for some amazing sunsets as you sip a cocktail and watch dolphins play in the harbour below.</p>
+                                <p className="card__readmore">read more</p>
+                            </div>
+                        </div>
+                        <p className="card__button">Book now</p>
+
+
                         {
                             hotels.map((hotel, index) =>
                                 <div key={index}>
@@ -30,6 +42,7 @@ const API_URL = "http://localhost:8888/get-establishments.php";
                                     <Link to={"/Hotelspesific/" + hotel.id} className="hotels--readmore">read more</Link>
                                     <Link to={"/Hotelspesific/" + hotel.id} className="hotels--button">Book now</Link>
                                 </div>
+
                             )
                         }
                     </div>
