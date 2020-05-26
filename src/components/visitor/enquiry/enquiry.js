c108f09405c3da4cbf7ef5dafc9690900b165655
Elise Myklebust <elisemykle@hotmail.com>
2020-05-26 15:31:10 +0200

    fjernet bootstrap fra form

--- src/components/visitor/enquiry/enquiry.js
+++ src/components/visitor/enquiry/enquiry.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { useForm } from "react-hook-form";
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
 import * as yup from "yup";
 import Hero from "../../Hero.js";
 
@@ -32,37 +30,37 @@ export default function Enquiry(props){
     function onSubmit() {
     }
     return(
-        <Form className="contact--form" onSubmit={handleSubmit(onSubmit)}>
-            <Form.Group>
+        <div className="contact" onSubmit={handleSubmit(onSubmit)}>
+            <div className="contact__page">
                 <Hero title="Enquiry" text="" classes="hero hero--contact" showSearch={false}/>
                 <div className="contact__contact--page">
-                    <h1 className="contact--h1">Make your reservation</h1>
+                    <h1 className="contact__h1">Make your reservation</h1>
                 </div>
-                <Form.Label className="form--label">Establishment:</Form.Label>
-                <Form.Control className="form--input" name="firstname" placeholder="Enter your first name" ref={register}/>
+                <label className="form__label">Establishment:</label>
+                <input className="form__input" name="firstname" placeholder="Enter your first name" ref={register}/>
                 {errors.firstname && <p className="error__message">Please enter minimum 2 characters.</p>}
-            </Form.Group>
-            <Form.Group>
-                <Form.Label className="form--label">Full name</Form.Label>
-                <Form.Control className="form--input" name="lastname" placeholder="Enter your last name" ref={register}/>
+            </div>
+            <div className="contact__page">
+                <label className="form__label">Full name</label>
+                <input className="form__input" name="lastname" placeholder="Enter your last name" ref={register}/>
                 {errors.lastname && <p className="error__message">Please enter minimum 2 characters.</p>}
-            </Form.Group>
-			<Form.Group>
-                <Form.Label className="form--label">Email adress</Form.Label>
-                <Form.Control className="form--input" name="emailadress" placeholder="Example@example.com" ref={register}/>
+            </div>
+			<div className="contact__page">
+                <label className="form__label">Email adress</label>
+                <input className="form__input" name="emailadress" placeholder="Example@example.com" ref={register}/>
                 {errors.emailadress && <p className="error__message">Please enter in a valid email format.</p>}
-            </Form.Group>
-            <Form.Group>
-                <Form.Label className="form--label">Check-in:</Form.Label>
-				<Form.Control className="form--input" type="date" name="date" ref={register}/>
+            </div>
+        	<div className="contact__page">
+                <label className="form__label">Check-in:</label>
+				<input className="form__input" type="date" name="date" ref={register}/>
 				{errors.emailadress && <p className="error__message">Please enter in a valid date.</p>}
-            </Form.Group>
-            <Form.Group>
-                <Form.Label className="form--label">Check-out:</Form.Label>
-				<Form.Control className="form--input" type="date" name="date" ref={register}/>
+            </div>
+            <div className="contact__page">
+                <label className="form__label">Check-out:</label>
+				<input className="form__input" type="date" name="date" ref={register}/>
 				{errors.emailadress && <p className="error__message">Please enter in a valid date.</p>}
-            </Form.Group>
-            <Button className="contact--button" type="submit">Send</Button>
-        </Form>
+            </div>
+            <button className="contact__button" type="submit">Send</button>
+        </div>
         )
 }
