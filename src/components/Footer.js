c82f9baeff5d02cebe2e497f4762fbd30d3f0715
Elise Myklebust <elisemykle@hotmail.com>
2020-05-27 19:06:10 +0200

    project reset

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Footer(props) {
-    return (
-        <div className="footer">
-            <p className="footer__logo"> © 2020 HOLIDAZE |  Copyright. All rights reserved.</p>
-        </div>
-    )
-}
--- src/components/admin/message/message.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function Message(props) {
-    return (
-        <h1 className="message__h1">Message</h1>
-    )
-}
