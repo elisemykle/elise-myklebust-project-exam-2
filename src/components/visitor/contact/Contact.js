a1912e143ff25c6fc56ceb4451899162b29f27f7
Elise Myklebust <elisemykle@hotmail.com>
2020-05-28 13:56:30 +0200

    style form

--- src/components/visitor/contact/Contact.js
+++ src/components/visitor/contact/Contact.js
@@ -32,7 +32,7 @@ export default function Contact(props){
 		history.push("/Success");
 	}
 	return(
-		<form className="contact__form" onSubmit={handleSubmit(onSubmit)}>
+		<form className="contact__form"  onSubmit={handleSubmit(onSubmit)}>
 			<div className="contact__page">
 				<Hero title="Contact us" text="" classes="hero hero--contact" showSearch={false}/>
 				<div className="contact__contact--page">
@@ -40,27 +40,35 @@ export default function Contact(props){
 					<p className="contact__text">Get in touch with our turist agency by completing the form down below. Providing you have any questions don’t hesitate to contact our team. We are always here to answer your questions.
 					</p>
 				</div>
-				<label className="form__label">First name</label>
-				<input className="form__input" name="firstname" placeholder="Enter your first name" ref={register}/>
-				{errors.firstname && <p className="error__message">Please enter minimum 2 characters.</p>}
+				<div className="col-6 col-m-12">
+					<label className="form__label">First name</label>
+					<input className="form__input" name="firstname" placeholder="Enter your first name" ref={register}/>
+					{errors.firstname && <p className="error__message">Please enter minimum 2 characters.</p>}
+				</div>
 			</div>
 
-			<div className="contact__page">
-				<label className="form__label">Last name</label>
-				<input className="form__input" name="lastname" placeholder="Enter your last name" ref={register}/>
-				{errors.lastname && <p className="error__message">Please enter minimum 2 characters.</p>}
+			<div className="col-6 col-m-12">
+				<div className="contact__page">
+					<label className="form__label">Last name</label>
+					<input className="form__input" name="lastname" placeholder="Enter your last name" ref={register}/>
+					{errors.lastname && <p className="error__message">Please enter minimum 2 characters.</p>}
+				</div>
 			</div>
 
-			<div className="contact__page">
-				<label className="form__label">Email adress</label>
-				<input className="form__input" name="emailadress" placeholder="Example@example.com" ref={register}/>
-				{errors.emailadress && <p className="error__message">Please enter in a valid email format.</p>}
+			<div className="col-12">
+				<div className="contact__page">
+					<label className="form__label">Email adress</label>
+					<input className="form__input" name="emailadress" placeholder="Example@example.com" ref={register}/>
+					{errors.emailadress && <p className="error__message">Please enter in a valid email format.</p>}
+				</div>
 			</div>
 
-			<div className="contact__page">
-				<label className="form__label">Message</label>
-				<input className="message__input" as="textarea" name="message" placeholder="Please enter your message here..." ref={register}/>
-				{errors.message && <p className="error__message">Please enter in minimum 10 characters.</p>}
+			<div className="col-12">
+				<div className="contact__page">
+					<label className="form__label">Message</label>
+					<input className="message__input" as="textarea" name="message" placeholder="Please enter your message here..." ref={register}/>
+					{errors.message && <p className="error__message">Please enter in minimum 10 characters.</p>}
+				</div>
 			</div>
 			<button className="contact__button" type="submit">Submit</button>
 		</form>
