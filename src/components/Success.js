8fc3867928d5f1a8d8d7eb19cbfbb615c2282f45
Elise Myklebust <elisemykle@hotmail.com>
2020-06-02 22:28:51 +0200

    La i kommentarer i css

--- src/components/Success.js
+++ src/components/Success.js
@@ -7,11 +7,11 @@ export default function Success(props) {
             <div className="col-6 col-m-12">
                 <h1 className="success__headline">Thank you!</h1>
                 <p className="success__message">We've received your message and we'll get back to your within 24 hours.</p>
-                <div className="container">
-                    <div className="center">
+
+
                         <Link to={"/"} className="success__button">Back to home</Link>
-                    </div>
-                </div>
+
+
             </div>
         </div>
     )
