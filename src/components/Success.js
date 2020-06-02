5cb052483820a6808f9969e8a180923f11265621
Elise Myklebust <elisemykle@hotmail.com>
2020-06-02 22:40:26 +0200

    Stylet success

--- src/components/Success.js
+++ src/components/Success.js
@@ -3,16 +3,12 @@ import { Link } from 'react-router-dom';
 
 export default function Success(props) {
     return (
-        <div className="row success">
-            <div className="col-6 col-m-12">
-                <h1 className="success__headline">Thank you!</h1>
-                <p className="success__message">We've received your message and we'll get back to your within 24 hours.</p>
-
-
-                        <Link to={"/"} className="success__button">Back to home</Link>
-
-
+        <div className="success">
+            <h1 className="success__headline">Thank you!</h1>
+            <p className="success__message">We've received your message and we'll get back to your within 24 hours.</p>
+            <div className="center">
+                <Link to={"/"} className="success__button">Back to home</Link>
             </div>
         </div>
-    )
+)
 }
