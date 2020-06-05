5030f553ea3fd9d926f2c6599aad62b10d68cd7e
Elise Myklebust <elisemykle@hotmail.com>
2020-06-05 15:44:51 +0200

    Success grid system

--- src/components/Success.js
+++ src/components/Success.js
@@ -4,8 +4,10 @@ import { Link } from 'react-router-dom';
 export default function Success(props) {
     return (
         <div className="success">
-            <h1 className="success__headline">Thank you!</h1>
+            <h1 className="row success__headline">Thank you!</h1>
+            <div className="col-6 col-m-12">
             <p className="success__message">We've received your submission and we'll get back to your within 24 hours.</p>
+            </div>
             <div className="col-12">
                 <Link to={"/"} className="success__button">Back to home</Link>
             </div>
